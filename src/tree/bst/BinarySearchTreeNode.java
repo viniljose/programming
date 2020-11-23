7ef16a45364454d99880084a7f6845c797b2226e
viniljose <vinilpj@gmail.com>
2020-11-23 07:01:13 +0530

    Create BinarySearchTreeNode.java
    BST first file

---
+++ src/tree/bst/BinarySearchTreeNode.java
@@ -0,0 +1,35 @@
+package tree.bst;
+
+public class BinarySearchTreeNode {
+    int val;
+    BinarySearchTreeNode left;
+    BinarySearchTreeNode right;
+    BinarySearchTreeNode() {}
+    BinarySearchTreeNode(int val) { this.val = val; }
+    BinarySearchTreeNode(int val, BinarySearchTreeNode left, BinarySearchTreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+
+    public static void main(String[] args) {
+        BinarySearchTreeNode bst = new BinarySearchTreeNode(7);
+        bst.left = new BinarySearchTreeNode(4);
+        bst.right = new BinarySearchTreeNode(9);
+        bst.left.left = new BinarySearchTreeNode(2);
+        bst.left.right = new BinarySearchTreeNode(5);
+
+        System.out.println(bst.findRecursive(bst,2).val);
+    }
+
+    public BinarySearchTreeNode findRecursive(BinarySearchTreeNode root,int data){
+        if(root == null)
+            return root;
+        if( data > root.val)
+            return findRecursive(root.right,data);
+        else if(data < root.val)
+            return findRecursive(root.left,data);
+        return root;
+
+    }
+}
