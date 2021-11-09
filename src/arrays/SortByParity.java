73d31f3824e07f4348ae95a48d382fc8c0400901
viniljose <vinilpj@gmail.com>
2021-11-09 09:22:33 +0530

    Create SortByParity.java

---
+++ src/arrays/SortByParity.java
@@ -0,0 +1,17 @@
+package arrays;
+
+/**
+ * https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3260/
+ * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+ * Input: nums = [3,1,2,4]
+ * Output: [2,4,3,1]
+ */
+public class SortByParity {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] sortArrayByParity(int[] nums) {
+        return null;
+    }
+}
