e0be395ea2c9789ebb77dcf67425d44d5888a8db
viniljose <vinilpj@gmail.com>
2021-11-09 09:55:42 +0530

    first impl

--- src/arrays/SortByParity.java
+++ src/arrays/SortByParity.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3260/
  * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
@@ -8,10 +10,32 @@ package arrays;
  */
 public class SortByParity {
     public static void main(String[] args) {
-
+        int[] nums = new int[] {3,1,2,4};
+        SortByParity sortByParity = new SortByParity();
+        sortByParity.sortArrayByParity(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
     public int[] sortArrayByParity(int[] nums) {
-        return null;
+        //check the number is even or odd
+        //if odd move to next element
+        //if even swap it with first element, then move to next
+        //Keep a tracker of even element position
+
+        // Check for edge cases.
+        if (nums == null || nums.length == 0 || nums.length == 1) {
+            return nums;
+        }
+        int tracker = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]%2 == 0){
+                int temp = nums[i];
+                nums[i]=nums[tracker];
+                nums[tracker] = temp;
+                tracker++;
+            }
+        }
+
+        return nums;
     }
 }
