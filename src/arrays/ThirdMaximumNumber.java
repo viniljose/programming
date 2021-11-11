6b3c9a39e47f5c8bb671b83b7e331776c925cc05
viniljose <vinilpj@gmail.com>
2021-11-11 09:22:44 +0530

    Create ThirdMaximumNumber.java

---
+++ src/arrays/ThirdMaximumNumber.java
@@ -0,0 +1,22 @@
+package arrays;
+
+/**
+ * https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3231/
+ * Given an integer array nums, return the third distinct maximum number in this array.
+ * If the third maximum does not exist, return the maximum number.
+ * Input: nums = [3,2,1]
+ * Output: 1
+ * Input: nums = [1,2]
+ * Output: 2
+ * Input: nums = [2,2,3,1]
+ * Output: 1
+ */
+public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public int thirdMax(int[] nums) {
+        return 0;
+    }
+}
