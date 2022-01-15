a0eb190d8029afe055be5fab21f467324bd01392
viniljose <vinilpj@gmail.com>
2022-01-15 17:32:57 +0530

    Update ValidPalindrome.java

--- src/strings/ValidPalindrome.java
+++ src/strings/ValidPalindrome.java
@@ -4,6 +4,10 @@ public class ValidPalindrome {
         System.out.println(palindrome.isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(palindrome.isPalindrome("race a car"));
         System.out.println(palindrome.isPalindrome(" "));
+        
+        System.out.println(palindrome.isPalindromeBetter("A man, a plan, a canal: Panama"));
+        System.out.println(palindrome.isPalindromeBetter("race a car"));
+        System.out.println(palindrome.isPalindromeBetter(" "));
     }
 
     //t:o(n) s:o(n)
@@ -25,5 +29,27 @@ public class ValidPalindrome {
         String reverse = builder.reverse().toString();
 
         return original.equals(reverse);
+    }
+    
+    //t:o(n) s:o(1)
+    public boolean isPalindromeBetter(String s) {
+        if(s == null)
+            return false;
+        if(s.equals(""))
+            return true;
+        if(s.length() ==1)
+            return true;
+
+        for(int i=0,j=s.length()-1;i<j;i++,j--){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while (i<j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+        }
+        return true;
     }
 }
