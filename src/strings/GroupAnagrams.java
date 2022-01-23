aaa4557b3195f5d5c62f79dcea77a411de5d520e
viniljose <vinilpj@gmail.com>
2022-01-23 17:51:10 +0530

    Create GroupAnagrams.java

---
+++ src/strings/GroupAnagrams.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        GroupAnagrams groupAnagrams = new GroupAnagrams();
+        System.out.println(groupAnagrams.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+        System.out.println(groupAnagrams.groupAnagrams(new String[]{""}));
+        System.out.println(groupAnagrams.groupAnagrams(new String[]{"a"}));
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> groups = new HashMap<>();
+
+        for(String s:strs){
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+
+            String key = String.valueOf(chars);
+            groups.putIfAbsent(key,new ArrayList<>());
+            groups.get(key).add(s);
+        }
+        return new ArrayList<>(groups.values());
+    }
+}
