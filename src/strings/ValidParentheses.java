d2a1b20ae184492326eb41f82ec69d48ea138632
viniljose <vinilpj@gmail.com>
2022-01-23 17:15:14 +0530

    Create ValidParentheses.java

---
+++ src/strings/ValidParentheses.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+
+        ValidParentheses vp = new ValidParentheses();
+        System.out.println(vp.isValid("()"));
+        System.out.println(vp.isValid("()[]{}"));
+        System.out.println(vp.isValid("(]"));
+        System.out.println(vp.isValid("([)]"));
+        System.out.println(vp.isValid("{[]}"));
+        System.out.println(vp.isValid(""));
+        System.out.println(vp.isValid("K"));
+    }
+
+    public boolean isValid(String s) {
+        if (s==null || s.length()==0)
+            return false;
+        HashMap<Character,Character> values = new HashMap<Character,Character>();
+        values.put('(',')');
+        values.put('{','}');
+        values.put('[',']');
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i=0;i<s.length();i++){
+            Character current = s.charAt(i);
+            if(values.containsKey(current)){
+                stack.push(current);
+            } else if (values.containsValue(current)){
+                if(!stack.isEmpty() && values.get(stack.peek())==current){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+
+            } else {
+                return false;
+            }
+        }
+      return stack.isEmpty();
+    }
+}
