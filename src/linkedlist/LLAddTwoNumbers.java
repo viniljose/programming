0db06ebf942dbe9b08f0207f0507af3abeccb4a4
viniljose <vinilpj@gmail.com>
2022-01-29 19:55:26 +0530

    Update LLAddTwoNumbers.java

--- src/linkedlist/LLAddTwoNumbers.java
+++ src/linkedlist/LLAddTwoNumbers.java
@@ -1,5 +1,8 @@
 package linkedlist;
-
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. 
+ * The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ */
 public class LLAddTwoNumbers {
 
     public static void main(String[] args) {
