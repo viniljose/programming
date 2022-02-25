8764e3e5961c81b1605ac7e34f2743eb3c930153
viniljose <vinilpj@gmail.com>
2022-02-25 15:46:53 +0530

    added helper methods

--- src/linkedlist/ListNode.java
+++ src/linkedlist/ListNode.java
@@ -11,6 +11,30 @@ public class ListNode {
         String result ="[";
         if(head == null)
             return "";
+        //commented for sentinel
+        //result = result + String.valueOf(head.val);
+        ListNode temp = head.next;
+        while (temp != null){
+            result = result +","+ String.valueOf(temp.val);
+            temp = temp.next;
+        }
+        return result +"]";
+    }
+
+    public int length(ListNode head){
+        int length =0;
+        ListNode curr = head;
+        while (curr!= null){
+            curr=curr.next;
+            length++;
+        }
+        return length;
+    }
+
+    public String toStringNoSentinel(ListNode head){
+        String result ="[";
+        if(head == null)
+            return "";
         result = result + String.valueOf(head.val);
         ListNode temp = head.next;
         while (temp != null){
