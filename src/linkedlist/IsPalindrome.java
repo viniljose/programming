d9dc48148ada0c36a1d8a24c7b9ca9276ac321e0
viniljose <vinilpj@gmail.com>
2022-03-06 19:00:03 +0530

    optimized solution

--- src/linkedlist/IsPalindrome.java
+++ src/linkedlist/IsPalindrome.java
@@ -12,6 +12,7 @@ public class IsPalindrome {
         ListNode head = new ListNode(1,node2);
         IsPalindrome isPalindrome = new IsPalindrome();
         System.out.println(isPalindrome.isPalindrome(head));
+        System.out.println(isPalindrome.isPalindromeFinal(head));
     }
 
     //o(n),o(n)
@@ -33,5 +34,42 @@ public class IsPalindrome {
             }
         }
         return true;
+    }
+
+    //o(n)o(1)
+    public boolean isPalindromeFinal(ListNode head) {
+        if(head == null) return true;
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null){
+            if(p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+    private ListNode endOfFirstHalf(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
     }
 }
