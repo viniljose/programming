fcc65ce0fa3405452a3fbf9a126e1cd02765339a
viniljose <vinilpj@gmail.com>
2022-03-19 22:32:13 +0530

    Iteration logic

--- src/tree/bst/ValidBST.java
+++ src/tree/bst/ValidBST.java
@@ -2,7 +2,13 @@ package tree.bst;
 
 import tree.TreeNode;
 
+import java.util.Stack;
+
 public class ValidBST {
+    private Stack<TreeNode> stack = new Stack<>();
+    private Stack<Integer> upperLimit = new Stack<>();
+    private Stack<Integer> lowerLimit = new Stack<>();
+
     public static void main(String[] args) {
         TreeNode tree =new TreeNode(10);
         tree.left = new TreeNode(5);
@@ -13,6 +19,7 @@ public class ValidBST {
 
         ValidBST validBST = new ValidBST();
         System.out.println(validBST.isValidBST(tree));
+        System.out.println(validBST.isValidBSTIterative(tree));
     }
 
     //Using Recursion
@@ -28,5 +35,33 @@ public class ValidBST {
             return false;
         }
         return validate(node.left,low,node.val) && validate(node.right,node.val,high);
+    }
+
+    private void add(TreeNode node,Integer low,Integer high){
+        stack.add(node);
+        lowerLimit.add(low);
+        upperLimit.add(high);
+    }
+
+
+    public boolean isValidBSTIterative(TreeNode root) {
+        Integer low = null;
+        Integer high = null;
+        Integer val = null;
+        add(root,low,high);
+        while (!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            low = lowerLimit.pop();
+            high = upperLimit.pop();
+            if(curr == null) continue;
+            val = curr.val;
+            if(low != null && val <= low)
+                return false;
+            if(high != null && val >= high)
+                return false;
+            add(curr.right,val,high);
+            add(curr.left,low,val);
+        }
+        return true;
     }
 }
