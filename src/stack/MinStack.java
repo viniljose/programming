1c55292aae577d9c081f94b425d6042d69a82eb1
viniljose <vinilpj@gmail.com>
2022-03-21 06:44:33 +0530

    Minimum of Stack

---
+++ src/stack/MinStack.java
@@ -0,0 +1,32 @@
+package stack;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<int[]> stack = new Stack<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if(stack.isEmpty()){
+            stack.push(new int[]{val,val});
+            return;
+        }
+        int currentMin = stack.peek()[1];
+        stack.push(new int[]{val,Math.min(val,currentMin)});
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
