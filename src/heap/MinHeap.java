4850f6012d051857d05a83d881dcfee2b619809e
viniljose <vinilpj@gmail.com>
2022-04-02 07:42:30 +0530

    added pop

--- src/heap/MinHeap.java
+++ src/heap/MinHeap.java
@@ -11,7 +11,7 @@ public class MinHeap {
         minHeap[0]=0;
     }
     public void add(int element){
-        if(size>capacity){
+        if(size==capacity){
             System.out.println("Heap is full");
             return;
         }
@@ -44,6 +44,42 @@ public class MinHeap {
         }
     }
 
+    public int peek(){
+        return minHeap[1];
+    }
+
+    public int pop(){
+        if(size<1){
+            System.out.println("Heap is Empty");
+            return Integer.MAX_VALUE;
+        } else {
+            int removeElement = minHeap[1];
+            int index = 1;
+            size--;
+            minHeap[1] = minHeap[size];
+            while (index<=size/2) {
+                int left = index * 2;
+                int right = index * 2 + 1;
+                if (minHeap[index] > minHeap[left] || minHeap[index] > minHeap[right]) {
+                    if (minHeap[left] < minHeap[right]) {
+                        int temp = minHeap[index];
+                        minHeap[index] = minHeap[left];
+                        minHeap[left] = temp;
+                        index = left;
+                    } else {
+                        int temp = minHeap[index];
+                        minHeap[index] = minHeap[right];
+                        minHeap[right] = temp;
+                        index = right;
+                    }
+                } else {
+                    break;
+                }
+            }
+            return removeElement;
+        }
+    }
+
     public static void main(String[] args) {
         // Test case
         MinHeap minHeap = new MinHeap(3);
@@ -53,15 +89,15 @@ public class MinHeap {
         // [1,3,2]
         System.out.println(minHeap.toString());
         // 1
-        //System.out.println(minHeap.peek());
+        System.out.println(minHeap.peek());
         // 1
-       // System.out.println(minHeap.pop());
+        System.out.println(minHeap.pop());
         // [2, 3]
-       // System.out.println(minHeap.toString());
-       // minHeap.add(4);
+        System.out.println(minHeap.toString());
+        minHeap.add(4);
         // Add too many elements
-       // minHeap.add(5);
+        minHeap.add(5);
         // [2,3,4]
-       // System.out.println(minHeap.toString());
+        System.out.println(minHeap.toString());
     }
 }
