76dfbb7feb8874b754bf5e8b3e80bf901bf4f3b9
viniljose <vinilpj@gmail.com>
2022-05-26 17:09:31 +0000

    Three Sum with Sort

---
+++ src/arrays/ThreeSum.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, 
+ * and nums[i] + nums[j] + nums[k] == 0.
+ * Notice that the solution set must not contain duplicate triplets.
+ * Input: nums = [-1,0,1,2,-1,-4] [[-1,-1,2],[-1,0,1]] * 
+ */
+public class ThreeSum {
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length && nums[i] <=0; i++) {
+            if(i==0 || nums[i]!=nums[i-1]){
+                twoSumII(res, i, nums);
+            }
+        }
+        return res;
+    }
+    private void twoSumII(List<List<Integer>> res,int i,int[] nums){
+        int lo=i+1,hi=nums.length-1;
+        while(lo < hi){
+            int sum = nums[i]+nums[lo]+nums[hi];
+            if(sum <0) lo++;
+            else if (sum >0 ) hi--;
+            else {
+                res.add(Arrays.asList(nums[i], nums[lo++], nums[hi--]));
+                while(lo < hi && nums[lo]==nums[lo-1]){
+                    lo++;
+                }
+            }
+        }
+    }
+}
