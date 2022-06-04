e68d75470e7c2785185b977b7d065ffadc878127
viniljose <vinilpj@gmail.com>
2022-06-04 13:40:14 +0000

    Jewels and Stones

---
+++ src/strings/JewelsAndStones.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelsSet = new HashSet<>();
+        for (char jewel : jewels.toCharArray()) {
+            jewelsSet.add(jewel);
+        }
+        int result = 0;
+        for(char stone:stones.toCharArray()){
+            if(jewelsSet.contains(stone))
+                result++;
+        }
+        return result;
+    }
+}
