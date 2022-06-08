3bacda9d9e017432d2e93548bed1d09b31cd8865
viniljose <vinilpj@gmail.com>
2022-06-08 03:30:18 +0000

    Is Graph a valid Tree

---
+++ src/graph/IsGraphAValidTree.java
@@ -0,0 +1,35 @@
+package graph;
+
+public class IsGraphAValidTree {
+    
+}
+import java.util.*;
+
+public class IsGraphAValidTree {
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+        Map<Integer,Integer> parent = new HashMap<>();
+        parent.put(0,-1);
+        Stack<Integer> stack= new Stack<>();
+        stack.add(0);
+        while (!stack.isEmpty()){
+            int node = stack.pop();
+            for(int neighbour:adjacencyList.get(node)){
+                if(parent.get(node)==neighbour)
+                    continue;
+                if(parent.containsKey(neighbour))
+                    return false;
+                stack.push(neighbour);
+                parent.put(neighbour,node);
+            }
+        }
+        return parent.size()==n;
+    }
+}
