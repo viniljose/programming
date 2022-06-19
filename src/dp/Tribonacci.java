4e5bd9dd5cab94b596a66c62f749ad5bac46acf8
viniljose <vinilpj@gmail.com>
2022-06-19 11:06:11 +0000

    Tribonacci Number

---
+++ src/dp/Tribonacci.java
@@ -0,0 +1,19 @@
+package dp;
+
+/**
+ * https://leetcode.com/problems/n-th-tribonacci-number/
+ */
+public class Tribonacci {
+    public int tribonacci(int n) {
+        if (n < 3) return n == 0 ? 0 : 1;
+
+        int tmp, x = 0, y = 1, z = 1;
+        for (int i = 3; i <= n; ++i) {
+            tmp = x + y + z;
+            x = y;
+            y = z;
+            z = tmp;
+        }
+        return z;
+    }
+}
