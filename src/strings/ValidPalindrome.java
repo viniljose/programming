4182009c017134e80e7ee669de3214e1bad2c6a3
viniljose <vinilpj@gmail.com>
2023-01-19 02:35:09 +0000

    adding comments

--- src/strings/ValidPalindrome.java
+++ src/strings/ValidPalindrome.java
@@ -1,4 +1,14 @@
 package strings;
+/**
+ * https://leetcode.com/problems/valid-palindrome/
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and
+ * removing all non-alphanumeric characters, it reads the same forward and backward.
+ * Alphanumeric characters include letters and numbers.
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ */
 public class ValidPalindrome {
     public static void main(String[] args) {
         ValidPalindrome palindrome = new ValidPalindrome();
