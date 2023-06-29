8695bc86ffa6d0bfb71726addc5dce3b603eea8b
viniljose <vinilpj@gmail.com>
2023-06-29 12:47:53 +0530

    description added

--- src/tree/DiameterOfBinaryTree.java
+++ src/tree/DiameterOfBinaryTree.java
@@ -1,5 +1,11 @@
 package tree;
 
+/**
+ * Given the root of a binary tree, return the length of the diameter of the tree.
+ *
+ * The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+ * This path may or may not pass through the root.
+ */
 public class DiameterOfBinaryTree {
     private int diameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
